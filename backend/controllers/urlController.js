68b644ccd73b88d1f1a60b85984570d3c589d7d3
Itai Hammer <itaihammer1@gmail.com>
2025-02-24 19:01:35 -0800

    Fixed controller structure

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -1,11 +1,11 @@
-export const shortenUrl = async (req, res) => {
+export async function shortenUrl(req, res) {
     // method that creates an object in the db with its url to redirect to
-};
+}
 
 export async function redirectUrl(req, res) {
     // finds the id in the db and redirects to the saved url
 }
 
-export const getUrlAnalytics = async (req, res) => {
+export async function getUrlAnalytics(req, res) {
     // finds the id in the db and returns the saved analytics
-};
+}
