6da59bf7c143c7f250091e7a1711c1335157eaed
Itai Hammer <itaihammer1@gmail.com>
2025-02-24 19:19:34 -0800

    ADDED nodemon AND ADDED confirmation root route

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -1,11 +1,14 @@
 export async function shortenUrl(req, res) {
     // method that creates an object in the db with its url to redirect to
+    console.log('Shorten Url');
 }
 
 export async function redirectUrl(req, res) {
     // finds the id in the db and redirects to the saved url
+    console.log('Redirect Url');
 }
 
 export async function getUrlAnalytics(req, res) {
     // finds the id in the db and returns the saved analytics
+    console.log('Get Url Analytics');
 }
