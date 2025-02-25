e77a02ec913605e60cfdd14dabe2d7702cd26b23
talberry <talberry05@gmail.com>
2025-02-24 19:34:58 -0800

    Added redirectUrl authentication

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -1,5 +1,11 @@
+import isUrlHttp from 'is-url-http';
+
 export async function shortenUrl(req, res) {
     // method that creates an object in the db with its url to redirect to
+    let {redirectUrl} = req.body;
+    if (!redirectUrl && !isUrlHttp(redirectUrl)) {
+        return res.status(400).json({error: 'Valid url is required'});
+    }
 }
 
 export async function redirectUrl(req, res) {
