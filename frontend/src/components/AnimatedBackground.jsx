83b42f8cb1fd37db368af4f17872c9f2f817f3a4
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2025-02-26 18:48:52 -0800

    Code cleanup

--- frontend/src/components/AnimatedBackground.jsx
+++ frontend/src/components/AnimatedBackground.jsx
@@ -1,93 +1,89 @@
-import React, { useMemo } from 'react';
-import { motion } from 'framer-motion';
+import React, { useMemo } from "react";
+import { motion } from "framer-motion";
 
-// Import your 9 scatter asset SVGs
-import Asset1 from '../icons/scatter/Asset 1.svg';
-import Asset2 from '../icons/scatter/Asset 2.svg';
-import Asset3 from '../icons/scatter/Asset 3.svg';
-import Asset4 from '../icons/scatter/Asset 4.svg';
-import Asset5 from '../icons/scatter/Asset 5.svg';
-import Asset6 from '../icons/scatter/Asset 6.svg';
-import Asset7 from '../icons/scatter/Asset 7.svg';
-import Asset8 from '../icons/scatter/Asset 8.svg';
-import Asset9 from '../icons/scatter/Asset 9.svg';
+import Asset1 from "../icons/scatter/Asset 1.svg";
+import Asset2 from "../icons/scatter/Asset 2.svg";
+import Asset3 from "../icons/scatter/Asset 3.svg";
+import Asset4 from "../icons/scatter/Asset 4.svg";
+import Asset5 from "../icons/scatter/Asset 5.svg";
+import Asset6 from "../icons/scatter/Asset 6.svg";
+import Asset7 from "../icons/scatter/Asset 7.svg";
+import Asset8 from "../icons/scatter/Asset 8.svg";
+import Asset9 from "../icons/scatter/Asset 9.svg";
 
 const assets = [
-    Asset1,
-    Asset2,
-    Asset3,
-    Asset4,
-    Asset5,
-    Asset6,
-    Asset7,
-    Asset8,
-    Asset9,
+  Asset1,
+  Asset2,
+  Asset3,
+  Asset4,
+  Asset5,
+  Asset6,
+  Asset7,
+  Asset8,
+  Asset9,
 ];
 
 const getRandomInt = (min, max) =>
-    Math.floor(Math.random() * (max - min + 1)) + min;
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
-const AnimatedBackground = () => {
-    const iconsCount = 40; // number of icons to render
+function AnimatedBackground() {
+  const iconsCount = 40;
 
-    const icons = useMemo(() => {
-        return Array.from({ length: iconsCount }).map((_, index) => {
-            // Randomly pick an asset
-            const asset = assets[getRandomInt(0, assets.length - 1)];
-            // Random horizontal position (0% to 100%)
-            const left = getRandomInt(0, 100);
-            // Starting vertical position off-screen (e.g., between 110% and 130%)
-            const startTop = getRandomInt(110, 130);
-            // Ending vertical position (above the top of the screen)
-            const endTop = -20;
-            // Animation duration between 10 and 30 seconds
-            const duration = getRandomInt(10, 30);
-            // Random delay between 0 and 5 seconds
-            const delay = Math.random() * 5;
-            // Random rotation angle between 0 and 360 degrees
-            const angle = getRandomInt(0, 360);
+  const icons = useMemo(() => {
+    // returns an array of randomly picked assets with random horizontal positions, random durations, and random rotations
 
-            return (
-                <motion.img
-                    key={index}
-                    src={asset}
-                    alt=""
-                    style={{
-                        position: 'absolute',
-                        left: `${left}%`,
-                        opacity: 0.15,
-                        width: '30px',
-                        height: '30px',
-                    }}
-                    initial={{ top: `${startTop}%`, rotate: angle }}
-                    animate={{ top: `${endTop}%` }}
-                    transition={{
-                        duration: duration,
-                        delay: delay,
-                        ease: 'linear',
-                        repeat: Infinity,
-                        repeatType: 'loop',
-                    }}
-                />
-            );
-        });
-    }, [iconsCount]);
+    return Array.from({ length: iconsCount }).map((_, index) => {
+      const asset = assets[getRandomInt(0, assets.length - 1)];
+      const left = getRandomInt(0, 100);
+      // Starting vertical position off-screen (e.g., between 110% and 130%)
+      const startTop = getRandomInt(110, 130);
+      // Ending vertical position (above the top of the screen)
+      const endTop = -20;
+      const duration = getRandomInt(10, 30);
+      const delay = Math.random() * 5;
+      const angle = getRandomInt(0, 360);
 
-    return (
-        <div
-            style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                pointerEvents: 'none', // so background doesn't block interactions
-                zIndex: -1, // behind other elements
-            }}
-        >
-            {icons}
-        </div>
-    );
-};
+      return (
+        <motion.img
+          key={index}
+          src={asset}
+          alt=""
+          style={{
+            position: "absolute",
+            left: `${left}%`,
+            opacity: 0.15,
+            width: "30px",
+            height: "30px",
+          }}
+          initial={{ top: `${startTop}%`, rotate: angle }}
+          animate={{ top: `${endTop}%` }}
+          transition={{
+            duration: duration,
+            delay: delay,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+        />
+      );
+    });
+  }, [iconsCount]);
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        pointerEvents: "none",
+        zIndex: -1,
+      }}
+    >
+      {icons}
+    </div>
+  );
+}
 
 export default React.memo(AnimatedBackground);
