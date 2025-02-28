d2b6a031a46346ba28fa9b765f79d96534a38d7a
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2025-02-27 23:28:00 -0800

    ADDED buy me a coffee button

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import urlRoutes from './routes/urlRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import urlRoutes from "./routes/urlRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.use(express.json());
 
-app.use('/api/url', urlRoutes);
+app.use("/api/url", urlRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
